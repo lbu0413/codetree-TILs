d5913b83eb744b2aceb98f579ecea44b8e597b2d
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-07 14:56:17 -0500

    Title: 좌표평면 위의 특정 구역 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231107/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 231107/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,21 +1,18 @@
 import sys
 
 min_ = sys.maxsize
-
 N = int(input())
-
 points = [tuple(map(int, input().split())) for _ in range(N)]
-
 
 for i in range(N):
     min_x, min_y, max_x, max_y = sys.maxsize, sys.maxsize, -sys.maxsize, -sys.maxsize
-    for j, (x, y) in enumerate(points):
+    for j in range(i, N):
         if i == j:
             continue
-        min_x = min(min_x, x)
-        min_y = min(min_y, y)
-        max_x = max(max_x, x)
-        max_y = max(max_y, y)
+        min_x = min(min_x, points[j][0])
+        min_y = min(min_y, points[j][1])
+        max_x = max(max_x, points[j][0])
+        max_y = max(max_y, points[j][1])
 
     area = (max_x - min_x) * (max_y - min_y)
     min_ = min(min_, area)
