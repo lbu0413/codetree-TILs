60cb989db5026ac17697e9dce07ef233e66fe563
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-07 12:04:41 -0500

    Title: 숫자 카운트, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231107/숫자 카운트/numeric-count.py
+++ 231107/숫자 카운트/numeric-count.py
@@ -1,16 +1,13 @@
 N = int(input())
 
 arr = [tuple(map(int, input().split())) for _ in range(N)]
-# arr = [tuple(map(int, input().split()))for _ in range(n)]
+
 
 ans = 0
 for i in range(1, 10):
     for j in range(1, 10):
         for k in range(1, 10):
             
-            if i == j or j == k or k == i:
-                continue
-
             for asked_num, cnt1, cnt2 in arr:
                 c1, c2 = 0, 0
                 x = asked_num // 100
