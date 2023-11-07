3ebf241c592a19b0557d4e45bcb5cc0a5cc4a0e5
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-07 17:39:57 -0500

    Title: 운행 되고 있는 시간, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 231107/운행 되고 있는 시간/hours-in-service.py
+++ 231107/운행 되고 있는 시간/hours-in-service.py
@@ -1,18 +1,29 @@
 import sys
 max_ = -sys.maxsize
-
 N = int(input())
-working_time = [tuple(map(int, input().split())) for _ in range(N)]
+
+MAX = 1000 + 1
+time = [tuple(map(int, input().split())) for _ in range(N)]
 
 
+
+# 해고 할 직원을 구한다
 for i in range(N):
-    total_ = 0
-    for j in range(N):
-        if i == j:
+    cnt = [0] * MAX
+    for j, (start, end) in enumerate(time):
+        if j == i:
             continue
-        start, end = working_time[j]
-        total_ += (end - start)
+        
+        # 나머지 직원들의 스케쥴을 카운트 배열에 집어넣는다.
+        for k in range(start, end):
+            cnt[k] += 1
+            
+    working_hours = 0    
+    for j in range(1, MAX):
+        if cnt[j] > 0:
+            working_hours += 1
+    
+    max_ = max(max_, working_hours)
 
-        max_ = max(max_, total_)
 
 print(max_)
