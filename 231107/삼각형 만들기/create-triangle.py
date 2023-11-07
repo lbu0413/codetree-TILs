1fb5918c03d7d2513d413f779dceb31f133e4d17
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-07 16:48:40 -0500

    Title: 삼각형 만들기, Time: 144ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231107/삼각형 만들기/create-triangle.py
+++ 231107/삼각형 만들기/create-triangle.py
@@ -7,7 +7,7 @@ dots = [tuple(map(int, input().split())) for _ in range(N)]
 def get_area(x1, y1, x2, y2, x3, y3):
     return ((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((x2 * y1) + (x3 * y2) + (x1 * y3))
 
-
+area = 0
 for i in range(N):
     for j in range(i+1, N):
         for k in range(j+1, N):
