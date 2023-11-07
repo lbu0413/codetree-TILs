163625838660f98085e008dec7887394a96afed0
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-07 13:04:53 -0500

    Title: 개발팀의 능력, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231107/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 231107/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -29,8 +29,8 @@ def get_diff(i, j, k, l):
 for i in range(N):
     for j in range(i+1, N):
         for k in range(N):
-            dup = 0
             for l in range(k+1, N):
+                dup = 0
                 if i == k or i == l or j == k or j == l:
                     continue
                 if duplicates(i, j, k, l):
