710bd1fa43f7e9f64c33ebb2472c65cd8b1ebc07
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-08 01:23:46 -0500

    Title: 스승의 은혜 2, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231108/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 231108/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -8,16 +8,19 @@ presents = [int(input()) for _ in range(N)]
 # 반 값으로 할인 받는 선물
 for i in range(N):
     cnt = 0
+    budget = 0
     for j in range(N): # 나머지 선물
         if j == i:
-            B -= (presents[j] // 2)
+            budget += presents[j]
             cnt += 1
             continue
+
+        budget += presents[j]
         
-        B -= presents[j]
+        if budget >= B:
+            break
+
         cnt += 1
 
-        if B <= 0:
-            break
     max_ = max(max_, cnt)
 print(max_)
