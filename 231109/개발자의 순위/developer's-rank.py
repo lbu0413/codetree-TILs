07b96ff295018fd529dea9988139cf83c90e6358
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-08 22:21:56 -0500

    Title: 개발자의 순위, Time: 118ms, Memory: 25MB, Status: Passed - CodeTree

--- 231109/개발자의 순위/developer's-rank.py
+++ 231109/개발자의 순위/developer's-rank.py
@@ -4,11 +4,11 @@ ranks = [list(map(int, input().split())) for _ in range(K)]
 
 pairs_count = 0
 
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
+for i in range(N):
+    for j in range(N):
         valid_pair = True
         for k in range(K):
-            if ranks[k].index(i) > ranks[k].index(j):
+            if ranks[k].index(i+1) > ranks[k].index(j+1):
                 valid_pair = False
                 break
         if valid_pair and i != j:
