bb5c0669466b9641bec88e1ce8d3fd2e9000b02f
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-08 22:41:55 -0500

    Title: 이상한 폭탄 2, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/이상한 폭탄 2/strange-bomb-2.py
+++ 231109/이상한 폭탄 2/strange-bomb-2.py
@@ -1,11 +1,20 @@
+import sys
+max_ = -sys.maxsize
+
 N, K = map(int, input().split())
 
 bombs = [int(input()) for _ in range(N)]
 
-max_ = -1
+
+explode = False
+
 for i in range(N):
     bombs_in_distance = bombs[i+1:i+K+1]
     if bombs[i] in bombs_in_distance:
-        max_ = max(bombs_in_distance)
+        explode = True
+        max_ = max(max_, bombs[i])
 
-print(max_)
+if explode:
+    print(max_)
+else:
+    print(-1)
