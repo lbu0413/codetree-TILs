274ea60703c46c86f02ec4f0646960ac750436a3
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-08 22:11:27 -0500

    Title: 개발자의 순위, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 231109/개발자의 순위/developer's-rank.py
+++ 231109/개발자의 순위/developer's-rank.py
@@ -1,23 +1,17 @@
-K, N = map(int ,input().split())
+K, N = map(int, input().split())
 
 ranks = [list(map(int, input().split())) for _ in range(K)]
 
-checker = []
-counter = [0] * (N + 1)
-for i in range(K):
-    for j in range(N):
-        for k in range(j+1, N):
-            greater_num = ranks[i][j]
-            smaller_num = ranks[i][k]
-            if i == 0:
-                checker.append((greater_num, smaller_num))
-                counter[greater_num] += 1
-            else:
-                if (greater_num, smaller_num) in checker:
-                    counter[greater_num] += 1
+pairs_count = 0
 
-ans = 0
-for i in counter:
-   ans += (i // K)
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        valid_pair = True
+        for k in range(K):
+            if ranks[k].index(i) > ranks[k].index(j):
+                valid_pair = False
+                break
+        if valid_pair and i != j:
+            pairs_count += 1
 
-print(ans)
+print(pairs_count)
