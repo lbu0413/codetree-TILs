ce237fe377915b7631b6464d6d0de1afa4ffac7d
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-09 00:08:03 -0500

    Title: 스승의 은혜 3, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 231109/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -10,10 +10,12 @@ for i in range(N):
     tmp[i][0] //= 2
     cnt = 0
     budget = 0
+    
     for j in range(N):
-        if budget + tmp[j][0] + tmp[j][1] <= B:
-            budget += tmp[j][0] + tmp[j][1]
-            cnt += 1
+        if budget + tmp[j][0] + tmp[j][1] > B:
+            break
+        budget += tmp[j][0] + tmp[j][1]
+        cnt += 1
 
     max_ = max(max_, cnt) 
     
