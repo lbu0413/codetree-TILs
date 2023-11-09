c947fb6094850db8f46f31a65ef84023f8035fd3
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-09 01:21:45 -0500

    Title: 숫자들의 합 중 최대, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 231109/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ 231109/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -3,11 +3,11 @@ ans = 0
 x, y = map(int, input().split())
 
 for i in range(x, y+1):
-    q = i // 10
-    r = i % 10
-
-    option = q+r
-    
-    max_ = max(max_, option)
+    sum_ = 0
+    while i:
+        sum_ += i % 10
+        i = i // 10
+    ans = sum_
+    max_ = max(max_, ans)
 
 print(max_)
