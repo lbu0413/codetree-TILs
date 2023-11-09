ef0ffde924b8e98f5da2b2d9cff7364173e109b2
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-09 00:07:14 -0500

    Title: 스승의 은혜 3, Time: 157ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 231109/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 231109/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -6,13 +6,13 @@ presents = [list(map(int, input().split())) for _ in range(N)]
 
 
 for i in range(N):
-    sorted_p = sorted(presents)
-    sorted_p[i][0] //= 2
+    tmp = [presents[k] for k in range(N)]
+    tmp[i][0] //= 2
     cnt = 0
     budget = 0
     for j in range(N):
-        if budget + presents[j][0] + presents[j][1] <= B:
-            budget += presents[j][0] + presents[j][1]
+        if budget + tmp[j][0] + tmp[j][1] <= B:
+            budget += tmp[j][0] + tmp[j][1]
             cnt += 1
 
     max_ = max(max_, cnt) 
