6b816ccb80e360d09e5d2f95329f0cd5e333d1a7
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-08 23:55:23 -0500

    Title: 스승의 은혜 3, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231109/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 231109/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -2,24 +2,16 @@ import sys
 
 max_ = -sys.maxsize
 N, B = map(int, input().split())
-presents = sorted([tuple(map(int, input().split())) for _ in range(N)])
+presents = ([tuple(map(int, input().split())) for _ in range(N)])
 
-for i in range(N): # 쿠폰을 쓸 선물을 고른다
-    gift, shipping = presents[i]
-    gift = gift // 2
-    total = gift + shipping
-    if total <= B:
-        cnt = 1
-    else:
-        cnt = 0
-    for j in range(N):
-        if i == j:
-            continue
-        if total + presents[j][0] + presents[j][1] <= B:
-            total += presents[j][0] + presents[j][1]
+for i in range(N):
+    cnt = 0
+    sorted_p = sorted(presents)
+    budget = (sorted_p[0][0] // 2) + (sorted_p[0][1])
+    for j in range(i + 1, N):
+        if budget + sorted_p[j][0] + sorted_p[j][1] <= B:
+            budget + sorted_p[j][0] + sorted_p[j][1]
             cnt += 1
     max_ = max(max_, cnt)
-        
-
 
 print(max_)
