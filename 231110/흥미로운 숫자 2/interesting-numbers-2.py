64edab0f753778d1b04654bffae60badd72c9e97
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-10 01:25:08 -0500

    Title: 흥미로운 숫자 2, Time: 148ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 231110/흥미로운 숫자 2/interesting-numbers-2.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+
+interesting_num_cnt = 0
+for i in range(a, b+1):
+    num = i
+    checker = [0] * 10
+    digit_cnt = 0
+    while num:
+        checker[num % 10] += 1
+        digit_cnt += 1
+        num = num // 10
+    
+    interesting_num = False
+    for j in range(8):
+        if checker[j] == digit_cnt - 1:
+            interesting_num = True
+    
+    if interesting_num:
+        interesting_num_cnt += 1
+
+print(interesting_num_cnt)
