c31ab0b45705b2b9e9e62e8b0bc8d717abe56022
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-09 19:24:37 -0500

    Title: 스승의 은혜 3, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231110/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,24 @@
+import sys
+
+max_ = 0
+N, B = map(int, input().split())
+presents = [list(map(int, input().split())) for _ in range(N)]
+
+
+for i in range(N):
+    tmp = [presents[k][:] for k in range(N)]
+    tmp.sort()
+    tmp[i][0] //= 2
+    cnt = 0
+    budget = 0
+
+    for j in range(N):
+        if budget + tmp[j][0] + tmp[j][1] > B:
+            break
+        budget += tmp[j][0] + tmp[j][1]
+        cnt += 1
+
+    max_ = max(max_, cnt) 
+    
+
+print(max_)
