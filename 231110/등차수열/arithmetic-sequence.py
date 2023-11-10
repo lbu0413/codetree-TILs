3180d26b532fcba21d095acef335b3e11386a28e
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-10 15:18:13 -0500

    Title: 등차수열, Time: 125ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 231110/등차수열/arithmetic-sequence.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+nums = list(map(int, input().split()))
+max_ = max(nums)
+
+cnt = 0
+for i in range(N):
+    for j in range(i+1, max_):
+        for k in range(i+1, N):
+            if abs(nums[i] - j) == abs(nums[k] - j):
+                cnt += 1
+
+print(cnt)
