6c2cc3383e4157b3d8267594e4bbb4b6548c8931
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-10 15:29:29 -0500

    Title: 등차수열, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231110/등차수열/arithmetic-sequence.py
+++ 231110/등차수열/arithmetic-sequence.py
@@ -3,13 +3,15 @@ N = int(input())
 nums = list(map(int, input().split()))
 max_ = 100 + 1
 
-cnt = 0
+ans = 0
 for k in range(1, max_):
+    cnt = 0
     for i in range(N):
         for j in range(i+1, N):
             if nums[i] + nums[j] == k * 2:
                 cnt += 1
 
+    ans = max(ans, cnt)
 
 
 print(cnt)
