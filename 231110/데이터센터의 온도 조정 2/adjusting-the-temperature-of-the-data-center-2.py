3982bceed6f778517be10b9581e7620f0d1182b4
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-10 00:33:03 -0500

    Title: 데이터센터의 온도 조정 2, Time: 150ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231110/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.py
@@ -0,0 +1,32 @@
+max_ = 1000
+
+n, c, g, h = map(int, input().split())
+ta, tb = [0] * n, [0] * n
+
+for i in range(n):
+    ta[i], tb[i] = tuple(map(int, input().split()))
+
+
+def single_efficiency(low, high, t):
+    if t < low:
+        return c
+    
+    elif t < high:
+        return g
+    
+    else:
+        return h
+
+
+def work_performance(t):
+    total_performance = 0
+    for i in range(n):
+        total_performance += single_efficiency(ta[i], tb[i], t)
+    return total_performance
+
+
+max_out = 0
+for t in range(max_ + 1):
+    max_out = max(max_out, work_performance(t))
+
+print(max_out)
