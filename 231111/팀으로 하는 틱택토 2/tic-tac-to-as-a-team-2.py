53b8a116af3c85c502ba00de2596d6ad6171bb48
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-11 17:06:40 -0500

    Title: 팀으로 하는 틱택토 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231111/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,60 @@
+MAX_A = 3
+MAX_X = 9
+
+ttt = [list(map(int, input())) for _ in range(MAX_A)]
+ans = 0
+
+for i in range(1, MAX_X + 1):
+    for j in range(i + 1, MAX_X + 1):
+        num_i, num_j = 0, 0
+        win = False
+    
+        # 가로로 한 줄을 맞췄을 때
+        for k in range(MAX_A):
+            num_i, num_j = 0, 0
+            for l in range(MAX_A):
+                if ttt[k][l] == i:
+                    num_i += 1
+                if ttt[k][l] == j:
+                    num_j += 1
+
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
+        
+        # 세로로 한 줄을 맞췄을 때
+        for k in range(MAX_A):
+            num_i, num_j = 0, 0
+            for l in range(MAX_A):
+                if ttt[l][k] == i:
+                    num_i += 1
+                
+                if ttt[l][k] == j:
+                    num_j += 1
+            
+            if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
+        
+        # 대각선 왼쪽 위에서 오른쪽 아래로
+        num_i, num_j = 0, 0
+        for k in range(MAX_A):
+            if ttt[k][k] == i:
+                num_i += 1
+            if ttt[k][k] == j:
+                num_j += 1
+        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+                win = True
+        
+        # 대각선 왼쪽 위에서 오른쪽 아래로
+        num_i, num_j = 0, 0
+        for k in range(MAX_A):
+            if ttt[MAX_A - k - 1][MAX_A - k - 1] == i:
+                num_i += 1
+            if ttt[MAX_A - k - 1][MAX_A - k - 1] == j:
+                num_j += 1
+        if num_i + num_j == 3 and num_i >= 1 and num_j >= 1:
+            win = True
+    
+        if win:
+            ans += 1
+
+print(ans)
