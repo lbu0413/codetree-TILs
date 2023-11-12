967004e10bf342ca2c32d6f05d9972190d4e3f44
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-12 12:32:41 -0500

    Title: 독서실의 거리두기 5, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 231112/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 231112/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,26 +1,21 @@
-import sys
-
-max_ = -sys.maxsize
-
 N = int(input())
-library = list(input())
+seats = list(input())
 
 
-
-def closest_ones():
-    closest = N
+def find_closest():
+    min_ = N
     for i in range(N):
         for j in range(i + 1, N):
-            if library[i] == '1' and library[j] == '1':
-                closest = min(closest, j - i)
-    return closest
+            if seats[i] == '1' and seats[j] == '1':
+                min_ = min(min_, j - i)
+    
+    return min_
 
-
+ans = 0
 for i in range(N):
-    if library[i] == '0':
-        library[i] = '1'
-        max_ = max(max_, closest_ones())
+    if seats[i] == '0':
+        seats[i] = '1'
+        ans = max(ans, find_closest())
+        seats[i] = '0'
 
-        library[i] = '0'
-
-print(max_)
+print(ans)
