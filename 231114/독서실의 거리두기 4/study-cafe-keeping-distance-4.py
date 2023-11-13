3429ec98309ab00fcadaa4249b2c85ec3294be46
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-13 16:15:40 -0500

    Title: 독서실의 거리두기 4, Time: 253ms, Memory: 26MB, Status: Passed - CodeTree

--- 231114/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 231114/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -1,6 +1,6 @@
 import sys
 
-max_ = -sys.maxsize
+
 
 N = int(input())
 seats = list(input())
@@ -8,20 +8,20 @@ seats = list(input())
 def distance_diff():
     min_ = sys.maxsize
     for i in range(N):
-        for j in range(i+1, N):
+        for j in range(i + 1, N):
             if seats[i] == '1' and seats[j] == '1':
-                distance = abs(j - i)
+                distance = (j - i)
                 min_ = min(min_, distance)
     return min_
 
 
-
+max_ = 0
 for i in range(N):
     for j in range(i + 1, N):
         if seats[i] == '0' and seats[j] == '0':
-            seats[i] == '1'
-            seats[j] == '1'
+            seats[i] = '1'
+            seats[j] = '1'
             max_ = max(max_, distance_diff())
             seats[i] = '0'
             seats[j] = '0'
-print(max_ - 1)
+print(max_)
