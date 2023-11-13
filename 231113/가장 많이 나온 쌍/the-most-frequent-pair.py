3819dfd86d36e8e3ef6511033e7e667e9d83a68b
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-12 21:29:16 -0500

    Title: 가장 많이 나온 쌍, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 231113/가장 많이 나온 쌍/the-most-frequent-pair.py
+++ 231113/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -5,7 +5,7 @@ n, m = map(int, input().split())
 pairs = [tuple(map(int, input().split())) for _ in range(m)]
 
 
-for i in range(n):
+for i in range(m):
     cnt = 0
     for a, b in pairs:
         if (pairs[i][0] == a and pairs[i][1] == b) or (pairs[i][0] == b and pairs[i][1] == a):
