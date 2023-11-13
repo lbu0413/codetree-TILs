cabf037303d33d2ce7583ec3473fa42318cd236e
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-13 12:35:51 -0500

    Title: 숫자들의 최대 차, Time: 272ms, Memory: 25MB, Status: Passed - CodeTree

--- 231114/숫자들의 최대 차/maximum-difference-in-numbers.py
+++ 231114/숫자들의 최대 차/maximum-difference-in-numbers.py
@@ -1,19 +1,30 @@
-import sys
+MAX_NUM = 10000
 
-max_ = -sys.maxsize
+# 선언 및 입력:
+n, k = tuple(map(int, input().split()))
+arr = [
+    int(input())
+    for _ in range(n)
+]
 
-N, K = map(int, input().split())
-nums = [int(input()) for _ in range(N)]
+# 구간 [l, r] 
+# 사이에 들어있는 숫자 개수를 반환합니다.
+def count_num(l, r):
+    cnt = 0
+    for elem in arr:
+        if l <= elem and elem <= r:
+            cnt += 1
 
-for i in range(N):
-    num = nums[i]
-    cnt = [num]
-    for j in range(N):
-        if i == j:
-            continue
-        if abs(nums[j] - max(cnt)) > K:
-            continue
-        else:
-            cnt.append(nums[j])
+    return cnt
 
-print(len(cnt))
+
+ans = 0
+# 크기가 K인 모든 구간을 잡아
+# 해당 구간 안에 들어오는 숫자의 개수를 세서
+# 그 중 최댓값을 계산합니다.
+for i in range(1, MAX_NUM + 1):
+    # 구간 [i, i + k] 사이에 들어있는 숫자를 세어
+    # 최댓값을 계산합니다.
+    ans = max(ans, count_num(i, i + k))
+
+print(ans)
