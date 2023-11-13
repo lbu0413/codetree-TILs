2aa94f440c1289212e0bace11876977a143b7c34
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-13 17:17:41 -0500

    Title: 초기 수열 복원하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231114/초기 수열 복원하기/restore-initial-sequence.py
@@ -0,0 +1,29 @@
+import sys
+
+N = int(input())
+MAX = 1000 + 1
+
+adjacent_sums = list(map(int, input().split()))
+arr = [0] * N
+
+for i in range(1, N + 1):
+    arr[0] = i
+    for j in range(1, N):
+        arr[j] = adjacent_sums[j - 1] - arr[j - 1]
+    
+    # 배열에는 1이상 N이하의 숫자들이 단 한번씩만 등장해야함.
+    satisfied = True
+    exists = [False] * MAX
+
+    for j in range(N):
+        if arr[j] < 1 or arr[j] > N:
+            satisfied = False
+        
+        if exists[arr[j]]:
+            satisfied = False
+        exists[arr[j]] = True
+    
+    if satisfied:
+        for x in arr:
+            print(x, end=" ")
+        sys.exit(0)
