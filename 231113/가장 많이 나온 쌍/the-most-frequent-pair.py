45a3674c9e5c2346dcd571ca31f9d223cbce09a7
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-12 21:28:53 -0500

    Title: 가장 많이 나온 쌍, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231113/가장 많이 나온 쌍/the-most-frequent-pair.py
@@ -0,0 +1,16 @@
+import sys 
+max_ = -sys.maxsize
+n, m = map(int, input().split())
+
+pairs = [tuple(map(int, input().split())) for _ in range(m)]
+
+
+for i in range(n):
+    cnt = 0
+    for a, b in pairs:
+        if (pairs[i][0] == a and pairs[i][1] == b) or (pairs[i][0] == b and pairs[i][1] == a):
+            cnt += 1
+    
+    max_ = max(max_, cnt)
+
+print(max_)
