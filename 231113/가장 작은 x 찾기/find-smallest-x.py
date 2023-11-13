416f22648fd5b70b9642f201eceb8ec027ae2d15
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-12 20:30:41 -0500

    Title: 가장 작은 x 찾기, Time: 115ms, Memory: 25MB, Status: Passed - CodeTree

--- 231113/가장 작은 x 찾기/find-smallest-x.py
+++ 231113/가장 작은 x 찾기/find-smallest-x.py
@@ -4,13 +4,15 @@ MAX_NUM = 10000
 n = int(input())
 conditions = [tuple(map(int, input().split()))for _ in range(n)]
 
-
-for x in range(1, MAX_NUM + 1):
-    tmp = x
+def satisfy(x):
     for a, b in conditions:
         x *= 2
-        if x < a or x > b:
-            break
-    else:
-        print(tmp)
+        if not (a <= x <= b):
+            return False
+    return True
+
+
+for x in range(1, MAX_NUM + 1):
+    if satisfy(x):
+        print(x)
         break
