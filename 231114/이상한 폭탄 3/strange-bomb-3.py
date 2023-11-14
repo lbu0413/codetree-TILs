738627aa7e8ebbc9fcacdd4374646ce006790e30
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-13 21:33:15 -0500

    Title: 이상한 폭탄 3, Time: 133ms, Memory: 32MB, Status: Passed - CodeTree

--- 231114/이상한 폭탄 3/strange-bomb-3.py
+++ 231114/이상한 폭탄 3/strange-bomb-3.py
@@ -6,7 +6,7 @@ bombs = [int(input()) for _ in range(N)]
 
 for i in range(N):
     the_bomb = bombs[i]
-    for j in range(i - K, K + 1):
+    for j in range(i - K, i + K + 1):
         if i == j:
             continue
         if j < 0 or j >= N:
@@ -14,11 +14,13 @@ for i in range(N):
         if the_bomb == bombs[j]:
             arr[bombs[j]] += 1
 
-
 max_bomb = max(arr)
 
 if max_bomb == 0:
     print(0)
 
 else:
-    print(arr.index(max_bomb))
+    for i in range(len(arr)):
+        if arr[i] == max_bomb:
+            ans = i
+    print(ans)
