989de71ddc32184e07c9f1843d4c79454ca3fe63
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-13 23:19:50 -0500

    Title: 언덕 깎기, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 231114/언덕 깎기/hill-cutting.py
+++ 231114/언덕 깎기/hill-cutting.py
@@ -1,17 +1,20 @@
+import sys
 N = int(input())
 
 hills = [int(input()) for _ in range(N)]
 
+MAX_H = 100
+MIN_ = sys.maxsize
+K = 17
 
-cost = 0
+for i in range(MAX_H):
+    cost = 0
+    for j in range(N):
+        if hills[j] < i:
+            cost += (i - hills[j]) * (i - hills[j])
+        
+        if hills[j] > i + K:
+            cost += ((i + K) - hills[j]) * ((i + K) - hills[j])
+    MIN_ = min(MIN_, cost)
 
-highest = max(hills)
-lowest = min(hills)
-diff = highest - lowest
-cost = 0
-while highest - lowest > 17:
-    highest -= 1
-    lowest += 1
-diff2 = highest - lowest
-
-print(abs(diff2 - diff) * 3)
+print(MIN_)
