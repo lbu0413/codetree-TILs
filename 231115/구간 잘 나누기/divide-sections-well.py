4a8777ad48b7e9628d4e26a5e54e2fa8dfe725b7
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-14 11:14:59 -0500

    Title: 구간 잘 나누기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231115/구간 잘 나누기/divide-sections-well.py
@@ -0,0 +1,17 @@
+import sys
+
+max_ = -sys.maxsize
+min_ = sys.maxsize
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+
+divider = n // m
+for i in range(0, n, divider):
+    cnt = 0
+    for j in range(i, i + divider):
+        cnt += nums[j]
+    max_ = max(max_, cnt)
+    # min_ = min(min_, max_)
+
+print(max_)
