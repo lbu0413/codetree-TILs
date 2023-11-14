01b7582d1b7eb1a33e09b1b4014d745f0f0d8fb0
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-14 17:38:58 -0500

    Title: 두 선분, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 231115/두 선분/two-lines.py
+++ 231115/두 선분/two-lines.py
@@ -1,8 +1,7 @@
 x1, x2, x3, x4 = map(int, input().split())
 
 
-
-if x3 < x2 or x1 < x4:
-    print("intersecting")
-else:
+if x2 < x3 or x4 < x1:
     print("nonintersecting")
+else:
+    print("intersecting")
