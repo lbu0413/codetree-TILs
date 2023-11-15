d482a6c637a3350b7cdeef1724fb5e87875eb398
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-15 16:06:48 -0500

    Title: 전부 겹치는 선분 2, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 231116/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -3,16 +3,12 @@ import sys
 N = int(input())
 lines = [tuple(map(int, input().split())) for _ in range(N)]
 
-def check_overlap(line1, line2):
-    return not(line1[1] < line2[0]) or not (line2[1] < line1[0])
-
-ans = "No"
 
 for i in range(N):
     for j in range(N - 1):
         if i == j:
             continue
-        if check_overlap(lines[j], lines[j + 1]):
+        if lines[j][1] < lines[j + 1][0] or lines[j + 1][1] < lines[j][0]:
             print("Yes")
             sys.exit()
 
