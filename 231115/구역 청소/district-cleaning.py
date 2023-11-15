4363e28d040195144d863b4abeda2c60f5e08d70
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-14 23:22:20 -0500

    Title: 구역 청소, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/구역 청소/district-cleaning.py
+++ 231115/구역 청소/district-cleaning.py
@@ -1,18 +1,13 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-N = max(a, b, c, d)
+if c < b:
+    ans = d - a
 
-arr = [0] * (N + 1)
-for i in range(a, b+1):
-    arr[i] = 1
+if a < d:
+    ans = b - c
 
-for j in range(c, d+1):
-    arr[j] = 1
+else:
+    ans = (b - a) + (d - c)
 
-cnt = 0
-for k in arr:
-    if k == 1:
-        cnt += 1
-
-print(cnt - 1)
+print(ans)
