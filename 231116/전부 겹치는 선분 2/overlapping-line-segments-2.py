7a2b68c4df3778114d57a92fa95f16be116a8bbe
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-15 16:07:43 -0500

    Title: 전부 겹치는 선분 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/전부 겹치는 선분 2/overlapping-line-segments-2.py
+++ 231116/전부 겹치는 선분 2/overlapping-line-segments-2.py
@@ -8,7 +8,7 @@ for i in range(N):
     for j in range(N - 1):
         if i == j:
             continue
-        if lines[j][1] < lines[j + 1][0] or lines[j + 1][1] < lines[j][0]:
+        if not (lines[j][1] < lines[j + 1][0]) or not(lines[j + 1][1] < lines[j][0]):
             print("Yes")
             sys.exit()
 
