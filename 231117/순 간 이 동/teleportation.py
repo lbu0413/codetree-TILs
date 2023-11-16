37d8dd09e42ca6e81bb6a6826dc703305ac020f5
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-16 18:22:29 -0500

    Title: 순 간 이 동, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

--- 231117/순 간 이 동/teleportation.py
+++ 231117/순 간 이 동/teleportation.py
@@ -4,7 +4,7 @@ min_ = sys.maxsize
 
 a, b, x, y = map(int, input().split())
 
-option1 = b - a
+option1 = abs(b - a)
 option2 = min(abs(x - a) + abs(y - b), abs(x - b) + abs(y - a))
 
 print(min(option1, option2))
