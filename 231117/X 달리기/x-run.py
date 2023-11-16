0aac5d8a1fd3eabc3b0a4b6586e3b802ef08dc3c
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-16 15:53:31 -0500

    Title: X 달리기, Time: 5036ms, Memory: 24MB, Status: Time Limit Exceed - CodeTree

--- 231117/X 달리기/x-run.py
+++ 231117/X 달리기/x-run.py
@@ -6,7 +6,7 @@ X = int(input())
 for i in range(1, X):
     distance, speed, time = 0, 0, 0
     while True:
-        if distance == X: # 목적지에 도착하면 종료
+        if distance == X and speed == 1: # 목적지에 도착하면 종료
             break
 
         if distance >= (X // 2): # 속도를 줄여야하는 지점에 진입
