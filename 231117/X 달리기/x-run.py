8934ca7ef7aa0ade112e69c8784996752d7635bc
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-16 16:08:40 -0500

    Title: X 달리기, Time: 161ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 231117/X 달리기/x-run.py
+++ 231117/X 달리기/x-run.py
@@ -3,13 +3,13 @@ import sys
 min_ = sys.maxsize
 X = int(input())
 
-for i in range(1, X):
+for i in range(X):
     distance, speed, time = 0, 0, 0
     while True:
         if distance == X and speed == 1: # 목적지에 도착하면 종료
             break
 
-        if distance >= (X // 2): # 속도를 줄여야하는 지점에 진입
+        if speed + distance > (X // 2): # 속도를 줄여야하는 지점에 진입
             if speed == 1:
                 speed = 1
             else:
@@ -19,7 +19,7 @@ for i in range(1, X):
         
         distance += speed
         time += 1
-
+    
     min_ = min(min_, time)
 
 print(min_)
