c43e8c18627c1f21ca147570b8b4ce808e5a74c0
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-16 01:07:14 -0500

    Title: 코딩톡, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231116/코딩톡/coding-talk.py
+++ 231116/코딩톡/coding-talk.py
@@ -1,25 +1,30 @@
 n, m, p = map(int, input().split())
-p -= 1
 messages = [tuple(map(str, input().split())) for _ in range(m)]
-
 alphabets = [chr(65 + i) for i in range(n)]
+p = p - 1
 
 people = ""
-for person, number in messages:
-    people += person
+for a, b in messages:
+    people += a
 
 # 읽지 않았을 가능성이 있는 사람들 = 
 # 나보다 메세지를 먼저 보낸 사람들 혹은 나 이후로 메세지를 보낸 적이 없는 사람들
+
+if p == 0:
+    print("")
+    exit()
+
 suspects = []
+for i in range(n):
+    if alphabets[i] not in people:
+        suspects.append(alphabets[i])
 
-for alph in alphabets:
-    if p == 0:
-        print("")
-        exit()
-    if alph not in people:
-        suspects.append(alph)
-    else:
-        if people.rindex(alph) < p:
-            suspects.append(alph)
-
-print(*suspects)
+    elif messages[i][1] == messages[p][1]:
+        continue
+    
+    elif people.rindex(messages[i][0]) < p:
+        suspects.append(messages[i][0])
+        
+    
+    
+print(*sorted(suspects))
