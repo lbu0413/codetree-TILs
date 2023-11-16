e11f40075bf79e5d9f3ddd053264995e536e81e2
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-15 22:37:52 -0500

    Title: 비둘기와 전기줄, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 231116/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 231116/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -2,18 +2,17 @@ N = int(input())
 
 pigeons = [tuple(map(int, input().split())) for _ in range(N)]
 
-check1 = ["X"] * 11
-check2 = [0] * 11
+check = [-1] * 11
 
+cnt = 0
 for i in range(N):
     pigeon, num = pigeons[i]
-    if (check1[pigeon] == 0 and num == 1) or (check1[pigeon] == 1 and num == 0):
-        check2[pigeon] += 1
-
-    elif check1[pigeon] == num:
-        continue
+    if check[pigeon] == -1:
+        check[pigeon] = num
     
-    check1[pigeon] = num
+    elif check[pigeon] != num:
+        check[pigeon] = num
+        cnt += 1
 
 
-print(sum(check2))
+print(cnt)
