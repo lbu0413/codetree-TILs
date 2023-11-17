c47cb9a832ace7762eca952301efa9a0cfc86d9e
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-17 02:19:18 -0500

    Title: 함수를 이용한 소수 판별, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231117/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+
+def prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+total = 0
+for i in range(a, b + 1):
+    if prime(i):
+        total += i
+
+print(total)
