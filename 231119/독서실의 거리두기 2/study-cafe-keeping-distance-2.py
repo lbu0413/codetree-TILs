11e09211d8aef8a60a647bb2e36a7dd9d35934f5
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-19 00:22:57 -0500

    Title: 독서실의 거리두기 2, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231119/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,29 @@
+import sys
+
+N = int(input())
+seats = list(input())
+
+min_ = sys.maxsize
+
+max_i, max_j = 0, 0
+diff = 0
+for i in range(N):
+    if seats[i] == '1':
+        for j in range(i + 1, N):
+            if seats[j] == '1':
+                if j - i > diff:
+                    diff = j - i
+                    max_i = i
+                    max_j = j
+                    break
+        seats[(max_i + max_j) // 2] = '1'
+
+
+for i in range(N):
+    if seats[i] == '1':
+        for j in range(i + 1, N):
+            if seats[j] == '1':
+                min_ = min(min_, j - i)
+                break
+
+print(min_)
