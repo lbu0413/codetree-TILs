d1f2deea7a15e8dff63a4b300026914b831d071d
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-19 02:01:27 -0500

    Title: ABC 줄 세우기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231119/ABC 줄 세우기/abc-line-up.py
+++ 231119/ABC 줄 세우기/abc-line-up.py
@@ -1,13 +1,18 @@
+import sys
 N = int(input())
 
+min_ = sys.maxsize
+
 people = list(input().split())
-cnt = 0
 
-for i in range(N):
-    for j in range(i + 1, N):
-        if people[i] > people[j]:
-            people[i], people[j] = people[j], people[i]
-            cnt += 1
+for k in range(N):
+    cnt = 0
+    for i in range(N):
+        for j in range(i + 1, N):
+            if people[i] > people[j]:
+                people[i], people[j] = people[j], people[i]
+                cnt += 1
             break
+    min_ = min(min_, cnt)
 
-print(cnt)
+print(min_)
