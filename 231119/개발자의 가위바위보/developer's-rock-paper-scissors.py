ba5bf1f130444dcc3b2f194f6867d4528a66f449
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-18 20:06:15 -0500

    Title: 개발자의 가위바위보, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231119/개발자의 가위바위보/developer's-rock-paper-scissors.py
+++ 231119/개발자의 가위바위보/developer's-rock-paper-scissors.py
@@ -3,28 +3,27 @@ N = int(input())
 rps = [(tuple(map(int, input().split()))) for _ in range(N)]
 
 
-first_max = 0
-second_max = 0
+win_one = 0
 for first, second in rps:
-    if first == second:
-        continue
-    
     if first == 1 and second == 2:
-        first_max += 1
-    
-    elif first == 2 and first == 1:
-        second_max += 1
-    
-    elif first == 1 and second == 3:
-        second_max += 1
-    
-    elif first == 3 and second == 1:
-        first_max += 1
+        win_one += 1
     
     elif first == 2 and second == 3:
-        first_max += 1
+        win_one += 1
+    
+    elif first == 3 and second == 1:
+        win_one += 1
+
+win_two = 0
+
+for first, second in rps:
+    if first == 1 and second == 3:
+        win_two += 1
     
     elif first == 3 and second == 2:
-        second_max += 1
+        win_two += 1
+    
+    elif first == 2 and second == 1:
+        win_two += 1
 
-print(max(first_max, second_max))
+print(max(win_one, win_two))
