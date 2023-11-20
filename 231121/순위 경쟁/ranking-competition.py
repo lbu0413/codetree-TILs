6b00893e6b4de7afdc1ade7204cce35903df3a76
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-20 15:51:15 -0500

    Title: 순위 경쟁, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231121/순위 경쟁/ranking-competition.py
+++ 231121/순위 경쟁/ranking-competition.py
@@ -1,38 +1,48 @@
 N = int(input())
-A, B, C = 0, 0, 0
-leader = ""
-cnt = 0
 
-for i in range(N):
-    person, score = map(str, input().split())
+scores = [tuple(map(str, input().split())) for _ in range(N)]
+a, b, c = 0, 0, 0
+
+number_of_leader_change = 0
+def get_scores(a, b, c):
+    if a > b and a > c: # A가 리더인 경우
+        return 1
+    
+    elif b > a and b > c: # B가 리더인 경우
+        return 2
+    
+    elif c > a and c > b: # c가 리더인 경우
+        return 3
+
+    elif a == b == c:
+        return 4
+    
+    elif a == b:
+        return 5
+    
+    elif a == c:
+        return 6
+    
+    elif b == c:
+        return 7
+
+
+for person, score in scores:
     score = int(score)
 
     if person == 'A':
-        A += score
+        if get_scores(a, b, c) != get_scores(a + score, b, c):
+            number_of_leader_change += 1
+            a += score
     
-    if person == 'B':
-        B += score
+    elif person == 'B':
+        if get_scores(a, b, c) != get_scores(a, b + score, c):
+            number_of_leader_change += 1
+            b += score
     
-    if person == 'C':
-        C += score
-    
-    current_max = max(A, B, C)
-    
-    if current_max == A and current_max ==  B  and current_max == C:
-        if leader != "":
-            cnt += 1
-        continue
-    
-    elif current_max == A and leader != 'A':
-        cnt += 1
-        leader = 'A'
-    
-    elif current_max == B and leader != 'B':
-        cnt += 1
-        leader = 'B'
-    
-    elif current_max == C and leader != 'C':
-        cnt += 1
-        leader = 'C'
+    else:
+        if get_scores(a, b, c) != get_scores(a, b, c + score):
+            number_of_leader_change += 1
+            c += score
 
-print(cnt)
+print(number_of_leader_change)
