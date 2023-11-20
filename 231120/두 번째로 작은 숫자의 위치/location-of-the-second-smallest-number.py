095199813d5454f10f76748102d1de14c966e8f8
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-19 23:42:29 -0500

    Title: 두 번째로 작은 숫자의 위치, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 231120/두 번째로 작은 숫자의 위치/location-of-the-second-smallest-number.py
+++ 231120/두 번째로 작은 숫자의 위치/location-of-the-second-smallest-number.py
@@ -6,6 +6,9 @@ min_ = tmp[0]
 
 for i in range(1, len(tmp)):
     if tmp[i] > min_:
+        if nums.count(tmp[i]) > 1:
+            print(-1)
+            exit()
         print(nums.index(tmp[i]) + 1)
         break
 
