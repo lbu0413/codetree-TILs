3efac65328578feff6abc83808f72f6fd0e46e62
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-20 15:11:25 -0500

    Title: 순위 경쟁, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231121/순위 경쟁/ranking-competition.py
@@ -0,0 +1,33 @@
+N = int(input())
+A, B, C = 0, 0, 0
+leader = ""
+cnt = 0
+
+for i in range(N):
+    person, score = map(str, input().split())
+    score = int(score)
+
+    if person == 'A':
+        A += score
+    
+    if person == 'B':
+        B += score
+    
+    if person == 'C':
+        C += score
+    
+    current_max = max(A, B, C)
+    
+    if current_max == A and leader != 'A':
+        cnt += 1
+        leader = 'A'
+    
+    elif current_max == B and leader != 'B':
+        cnt += 1
+        leader = 'B'
+    
+    elif current_max == C and leader != 'C':
+        cnt += 1
+        leader = 'C'
+
+print(cnt)
