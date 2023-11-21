a8abda723b73115a2dcb7ef79a62ab161b673d03
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-20 23:12:49 -0500

    Title: 홀수 짝수의 묶음, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231121/홀수 짝수의 묶음/odd-even-bundle.py
@@ -0,0 +1,40 @@
+N = int(input())
+nums = list(map(int, input().split()))
+
+even, odd = 0, 0
+
+for i in nums:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+
+group_num = 0
+while True:
+
+    if group_num % 2 == 0:
+        if even:
+            even -= 1
+            group_num += 1
+        
+        elif odd >= 2:
+            odd -= 2
+            group_num += 1
+        
+        else:
+            if even > 0 or odd > 0:
+                group_num -= 1
+
+            break
+    
+
+    else:
+        if odd:
+            odd -= 1
+            group_num += 1
+        
+        else:
+            break
+
+print(group_num)
