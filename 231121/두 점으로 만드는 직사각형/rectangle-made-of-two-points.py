9dbb60db66f2b2be38990464fca7877e0b1840c5
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-21 02:56:54 -0500

    Title: 두 점으로 만드는 직사각형, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231121/두 점으로 만드는 직사각형/rectangle-made-of-two-points.py
@@ -0,0 +1,15 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+
+
+min_x = min(x1, x2, a1, a2)
+max_x = max(x1, x2, a1, a2)
+min_y = min(y1, y2, b1, b2)
+max_y = max(y1, y2, b1, b2)
+
+
+x = (max_x - min_x)
+y = (max_y - min_y)
+
+print(x * y)
