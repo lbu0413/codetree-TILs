a95ac5b5ba86e5cc1f409f007b8dadd175d91eae
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-20 22:05:05 -0500

    Title: 최소 와이파이 수, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 231121/최소 와이파이 수/minimum-number-of-wifi.py
+++ 231121/최소 와이파이 수/minimum-number-of-wifi.py
@@ -5,17 +5,17 @@ nums = list(map(int, input().split()))
 # 와이파이 한 대가 커버할 수 있는 범위
 cover = 2 * M + 1
 
-if M == 0:
-    print(nums.count(1))
+cnt, i = 0, 0
 
-elif N == nums.count(0):
-    print(0)
-
-
-else:
-    if cover >= N:
-        ans = 1
+while True:
+    if i >= N:
+        break
+    
+    if nums[i] == 1:
+        cnt += 1
+        i += 2 * M + 1
+    
     else:
-        ans = N // cover + N % cover
+        i += 1
 
-    print(ans)
+print(cnt)
