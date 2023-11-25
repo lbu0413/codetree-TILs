1f0db0336c9d64050bde1b3b8e2bb7b8d5f779e6
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-25 01:35:12 -0500

    Title: 힙 정렬 구현, Time: 749ms, Memory: 33MB, Status: Passed - CodeTree

--- 231125/힙 정렬 구현/implement-heap-sort.py
+++ 231125/힙 정렬 구현/implement-heap-sort.py
@@ -7,10 +7,10 @@ def heapify(N, i):
     left = i * 2
     right = i * 2 + 1
 
-    if left <= N and arr[left] > arr[largest]:
+    if left < N and arr[left] > arr[largest]:
         largest = left
     
-    if right <= N and arr[right] > arr[largest]:
+    if right < N and arr[right] > arr[largest]:
         largest = right
     
     if largest != i:
@@ -18,14 +18,14 @@ def heapify(N, i):
         heapify(N, largest)
 
 def heapsort():
-    for i in range(N//2, 0 -1):
+    for i in range(N//2, -1, -1):
         heapify(N, i)
     
-    for i in range(N, 1, -1):
-        arr[1], arr[i] = arr[i], arr[1]
-        heapify(i - 1, 1)
+    for i in range(N-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(i, 0)
         
 
 heapsort()
 
-print(*arr[1:])
+print(*arr)
