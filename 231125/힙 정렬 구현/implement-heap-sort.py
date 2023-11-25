2f09e3f06e4d7c001ce2e5df4295f18c5a38b525
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-25 02:05:48 -0500

    Title: 힙 정렬 구현, Time: 954ms, Memory: 33MB, Status: Passed - CodeTree

--- 231125/힙 정렬 구현/implement-heap-sort.py
+++ 231125/힙 정렬 구현/implement-heap-sort.py
@@ -21,7 +21,7 @@ def heapsort():
     for i in range(N//2, -1, -1):
         heapify(N, i)
     
-    for i in range(N-1, 0, -1):
+    for i in range(N-1, -1, -1):
         arr[0], arr[i] = arr[i], arr[0]
         heapify(i, 0)
         
