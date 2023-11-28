781991841f02af3a4a4a7d555a3fc232b7ec52fa
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-27 22:34:51 -0500

    Title: 최고의 33위치, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231128/최고의 33위치/best-place-of-33.py
+++ 231128/최고의 33위치/best-place-of-33.py
@@ -9,22 +9,19 @@ grid = [list(map(int, input().split())) for _ in range(N)]
 
 def get_coins(row, col, new_row, new_col):
     cnt = 0
-
-    for i in range(row, new_row):
-        for j in range(col, new_col):
+    for i in range(row, new_row + 1):
+        for j in range(col, new_col + 1):
             if grid[i][j] == 1:
                 cnt += 1
     return cnt
 
 for row in range(N):
     for col in range(N):
-        for k in range(1, 4):
-            new_row = row + k
-            new_col = col + k
-            if 0 <= new_row < N and 0 <= new_col < N:
-                coins = get_coins(row, col, new_row, new_col)
-                max_ = max(max_, coins)
-
-            else:
-                continue
+        new_row = row + 2
+        new_col = col + 2
+        if 0 <= new_row < N and 0 <= new_col < N:
+            coins = get_coins(row, col, new_row, new_col)
+    
+    max_ = max(max_, coins)
+        
 print(max_)
