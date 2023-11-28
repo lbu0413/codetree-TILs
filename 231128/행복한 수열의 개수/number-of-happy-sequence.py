0969c7260c25cd362f300f4089d8ae47b9129cbd
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-11-28 00:33:42 -0500

    Title: 행복한 수열의 개수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231128/행복한 수열의 개수/number-of-happy-sequence.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+
+grid = [list(map(int, input().split())) for _ in range(N)]
+
+cnt = 0
+
+for i in range(N):
+    row = grid[i]
+    permutation_cnt = 1
+    for j in range(N - 1):
+        if row[j] == row[j + 1]:
+            permutation_cnt += 1
+        
+        if permutation_cnt == M:
+            cnt += 1
+            break
+
+
+for i in range(N):
+    permutation_cnt = 1
+    for j in range(N-1):
+        if grid[j][i] == grid[j + 1][i]:
+            permutation_cnt += 1
+        
+        if permutation_cnt == M:
+            cnt += 1
+            break
+
+print(cnt)
