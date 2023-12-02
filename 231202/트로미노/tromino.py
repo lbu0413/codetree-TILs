ae263e2c196f318c2be352eefe9f64f70d0f3399
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-02 00:32:29 -0500

    Title: 트로미노, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231202/트로미노/tromino.py
+++ 231202/트로미노/tromino.py
@@ -20,20 +20,15 @@ for i in range(n):
         max_ = max(max_, vertical)
 
 
-for i in range(n-1):
-    for j in range(m-1):
-        one, two, three, four = 0, 0, 0, 0
+min_ = 1001
+for i in range(n):
+    for j in range(m):
+        square = 0
         for k in range(2):
-            if 0 <= i+k < n and 0 <= j+k < m:
-                one += grid[i+1][j+k]
-                two += grid[i+1][j+k]
-                three += grid[i][j+k]
-                four += grid[i][j+k]
-        
-        one += grid[i][j]
-        two += grid[i][j+1]
-        three += grid[i][j+1]
-        four += grid[i+1][j]
-        max_ = max(max_, one, two, three, four)
-
+            for l in range(2):
+                if 0 <= i+k < n and 0 <= j+l < m:
+                    square += grid[i+k][j+l]
+                    min_ = min(min_, grid[i+k][j+l])
+        square -= min_
+max_ = max(max_, square)
 print(max_)
