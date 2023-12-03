7c95cd6a3636fe999e30f380fe91bf8a3f27d6f3
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-03 11:32:34 -0500

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 231204/데이터 교환/exchange-data.cpp
+++ 231204/데이터 교환/exchange-data.cpp
@@ -4,15 +4,13 @@ int main() {
     // 여기에 코드를 작성해주세요.
 
     int a = 5, b = 6, c = 7;
-    int tmp, tmp2;
+    int tmp;
 
-    tmp = b;
-    tmp2 = c;
+    tmp = a;
 
-    b = a;
-    c = tmp;
-    a = tmp2;
-
+    a = c;
+    c = b;
+    b = tmp;
 
     cout << a << endl;
     cout << b << endl;
