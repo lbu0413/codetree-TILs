bcb52a3747307bd2dc5c7586bce60b61a9799191
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-04 23:09:27 -0500

    Title: 실수와 문자 받아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231205/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    char c;
+    double a, b;
+    
+    cout << fixed;
+    cout.precision(2);
+    cin >> c >> a >> b;
+    cout << c << endl;
+    cout << a << endl;
+    cout << b;
+    return 0;
+}
