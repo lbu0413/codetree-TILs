396f01c133d658b8bbb2329b77a0fe5abbdbeeb5
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-04 23:26:12 -0500

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231205/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int a, b, c;
+    char d;
+
+    cin >> a >> d >> b >> d >> c;
+    cout << '0' << a << d << c << d << b;
+    return 0;
+}
