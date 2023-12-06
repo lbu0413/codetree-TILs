4093e171d0d5a452fd86ba2402366de448d3f61c
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-06 14:43:37 -0500

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231207/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int budget;
+    cin >> budget;
+
+    if (budget >= 3000) {
+        cout << "book";
+    }
+    else if (budget >= 1000) {
+        cout << "mask";
+    }
+    else if (budget >= 500) {
+        cout << "pen";
+    }
+    else {
+        cout << "no";
+    }
+    return 0;
+}
