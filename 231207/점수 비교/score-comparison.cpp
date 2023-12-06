511fcf45e17682d8850beb1a598a53869eb4d59c
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-06 16:21:36 -0500

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231207/점수 비교/score-comparison.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int math_a, eng_a;
+    int math_b, eng_b;
+
+    cin >> math_a >> eng_a;
+    cin >> math_b >> eng_b;
+
+    cout << (math_a > math_b && eng_a > eng_b);
+
+    return 0;
+}
