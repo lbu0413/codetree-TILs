29e363ce089458d50942b1c524feb0f630aeb222
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-07 17:27:49 -0500

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231208/코로나 메뉴얼/covid-manual.cpp
+++ 231208/코로나 메뉴얼/covid-manual.cpp
@@ -10,27 +10,24 @@ int main() {
     cin >> s1 >> t1;
     cin >> s2 >> t2;
     cin >> s3 >> t3;
-    
-    if (s1 == 'Y' && t1 >= 37) {
-        A += 1;
-    }
-    
-    if (s2 == 'Y' && t2 >= 37) {
-        A += 1;
-    }
-    
-    if (s3 == 'Y' && t3 >= 37) {
-        A += 1;
-    }
 
-    if (A >= 2) {
-        cout << 'E';
+    if (s1 == 'Y' && t1 >= 37) {
+        if ((s2 == 'Y' && t2 >= 37) || (s3 == 'Y' && t3 >= 37)) {
+            cout << 'E';
+        }
+        else {
+            cout << 'N';
+        }
     }
     else {
-        cout << 'N';
+        if ((s2 == 'Y' && t2 >= 37) && (s3 == 'Y' && t3 >= 37)) {
+            cout << 'E';
+        }
+        else {
+            cout << 'N';
+        }
     }
 
     
-
     return 0;
 }
