9200d06fd76444d561ed9369bcfccc87328bdbe0
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-07 17:19:44 -0500

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231208/코로나 메뉴얼/covid-manual.cpp
+++ 231208/코로나 메뉴얼/covid-manual.cpp
@@ -5,21 +5,51 @@ int main() {
     
     char s1, s2, s3;
     int t1, t2, t3;
-    int A;
+    int A, B, C, D;
 
-    cin >> s1 >> t1 >> s2 >> t2 >> s3 >> t3;
+    cin >> s1 >> t1;
+    cin >> s2 >> t2;
+    cin >> s3 >> t3;
 
     if (s1 == 'Y' && t1 >= 37) {
         A += 1;
     }
-    
-    else if (s2 == 'Y' && t2 >= 37) {
-        A += 1;
+    else if (s1 == 'N' && t1 >= 37) {
+        B += 1;
+    }
+    else if (s1 == 'Y' && t1 < 37) {
+        C += 1;
+    }
+    else {
+        D += 1;
     }
 
-    else if (s3 == 'Y' && t3 >= 37) {
+    if (s2 == 'Y' && t2 >= 37) {
         A += 1;
     }
+    else if (s2 == 'N' && t2 >= 37) {
+        B += 1;
+    }
+    else if (s2 == 'Y' && t2 < 37) {
+        C += 1;
+    }
+    else {
+        D += 1;
+    }
+
+    if (s3 == 'Y' && t3 >= 37) {
+        A += 1;
+    }
+    else if (s3 == 'N' && t3 >= 37) {
+        B += 1;
+    }
+    else if (s3 == 'Y' && t3 < 37) {
+        C += 1;
+    }
+    else {
+        D += 1;
+    }
+
 
     if (A >= 2) {
         cout << 'E';
