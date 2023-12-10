5e9689e860c8d68d25edb0ee90a55011b3e14709
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-09 20:31:28 -0500

    Title: 입력받은 수의 합과 평균, Time: 14ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231210/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    double avg;
+
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+
+        sum += num;
+    }
+
+    cout << fixed;
+    cout.precision(1);
+
+    avg = (double)sum / n;
+
+    cout << sum << " " << avg;
+    return 0;
+}
