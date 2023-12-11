ed2b2b332195b764c82de5af717dad909c7663db
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-10 23:00:47 -0500

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231211/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    while (true) {
+        int num;
+        cin >> num;
+        if (num == 0) {
+            break;
+        }
+        cout << num << endl;
+    }
+    return 0;
+}
