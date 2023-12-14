9212c175e1c71b829e8480325fac9c8653101d02
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2023-12-14 16:54:11 -0500

    Title: 합성수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231215/합성수 판별기/factorization-discriminator.cpp
+++ 231215/합성수 판별기/factorization-discriminator.cpp
@@ -4,17 +4,14 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
+    bool prime = false;
     for (int i = 2; i < n; i++) {
         if (n % i == 0) {
-            cout << 'C';
-            break;
-        }
-        else {
-            cout << 'N';
-            break;
+            prime = true;
         }
     }
+
+    cout << (prime ? 'C' : 'N');
 
     return 0;
 }
