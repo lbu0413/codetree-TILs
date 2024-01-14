40c4fa0b55fb213e0bb0759df1ea74dc90b34cc4
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2024-01-14 12:32:50 -0500

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240115/별표 출력하기 3/print-star-3.cpp
+++ 240115/별표 출력하기 3/print-star-3.cpp
@@ -9,7 +9,7 @@ int main() {
         for (int j = 0; j < i; j++) {
             cout << "  ";
         }
-        for (int j = (2 * a) - (2 * i) - 1; j > 0; j--) {
+        for (int j = 0; j < (2 * a) - (2 * i) - 1; j++) {
             cout << "* ";
         }
         cout << endl;
