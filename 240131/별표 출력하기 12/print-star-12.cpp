9a209a482bd6d4bf67c5a96c5870e0890a58019a
Wook Lee <66400448+lbu0413@users.noreply.github.com>
2024-01-30 14:05:54 -0500

    Title: 별표 출력하기 12, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240131/별표 출력하기 12/print-star-12.cpp
+++ 240131/별표 출력하기 12/print-star-12.cpp
@@ -7,7 +7,7 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (i == 0 || j == n-1) {
+            if (i == 0) {
                 cout << "* ";
             }
             else if (j % 2 != 0 && j >= i) {
